680d6394fcc63bac918616f52c4d47ccbe6460da
Roman Franko <roma.franko99@gmail.com>
2021-09-09 20:54:44 +0300

    First bild

---
+++ src/components/app/app.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Header from "../header";
+import Layout from "../layout";
+import Footer from "../footer";
+
+import imgBG from "../../assets/bg.jpg"
+import jucyBG from "../../assets/bg1.jpg"
+
+const App = () => {
+    return (
+        <>
+            <Header title={"Title"} descr={"Description"}/>
+            <Layout title={"Test title #1"} descr={"Description"} urlBg={imgBG}/>
+            <Layout title={"Test title #2"} descr={"Description"} colorBg={"#78c594"}/>
+            <Layout title={"Test title #3"} descr={"Description"} urlBg={jucyBG}/>
+            <Footer/>
+        </>
+    )
+}
+
+export default App;
